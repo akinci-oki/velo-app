e3ade3bfce230ae0f8790ff7deba43bb3a516607
Okan Akıncı <okan@akinci.be>
2022-11-15 22:19:39 +0300

    call api

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect } from "react";
+import axios from "axios";
 
 function Map (): JSX.Element {
+    async function getLocations (): Promise<void> {
+        try {
+            const response = await axios.get("http://api.citybik.es/v2/networks/velo-antwerpen");
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect((): void => {
+        getLocations();
+    }, []);
+
     return (
         <MapContainer
             center={[51.2213, 4.4051]}
@@ -19,5 +34,7 @@ function Map (): JSX.Element {
         </MapContainer>
     );
 }
+
+/*http://api.citybik.es/v2/networks/velo-antwerpen*/
 
 export default Map;
