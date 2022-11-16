ff40d4e458c263703d6f17cc3b148086a4fa5afe
Okan Akıncı <okan@akinci.be>
2022-11-16 13:16:09 +0300

    multiple markers

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,9 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Map (): JSX.Element {
+    const [markerPositions, setMarkerPositions] = useState([[51.22373, 4.40302], [51.22219, 4.40459]] as Array<[number, number]>);
     async function getLocations (): Promise<void> {
         try {
             const response = await axios.get("http://api.citybik.es/v2/networks/velo-antwerpen");
@@ -20,17 +21,19 @@ function Map (): JSX.Element {
         <MapContainer
             center={[51.2213, 4.4051]}
             zoom={12.4}
-            scrollWheelZoom={false}
+            scrollWheelZoom={true}
         >
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
+            {markerPositions.map((marker, index) => (
+                <Marker key={index} position={marker}>
+                    <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+                    </Popup>
+                </Marker>
+            ))};
         </MapContainer>
     );
 }
