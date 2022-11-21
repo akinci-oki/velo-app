6284299e63ec96bf792eac09a2b4b84953fcc6fd
Okan Akıncı <okan@akinci.be>
2022-11-21 19:34:51 +0300

    styling the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import "./App.scss";
-import Map from "./components/Map/Map";
 import Home from "components/Homepage/Home";
 
 function App (): JSX.Element {
     return (
         <div className="App">
             <Home></Home>
-            <Map></Map>
         </div>
     );
 }
--- src/components/Homepage/Home.tsx
+++ src/components/Homepage/Home.tsx
@@ -1,7 +1,35 @@
+import Map from "components/Map/Map";
+
 function Homepage (): JSX.Element {
     return (
         <div className="home-page">
-            <h1 className="header"> Pick a station </h1>
+            <div className="buttons">
+                <button>
+                    <img className="icon" src="icons/user-icon-white.png" />
+                </button>
+            </div>
+            <div>
+                <div className="bike-image" />
+                <div className="text-space">
+                    <p>
+                        Find a Velo station and save your favourite stations.
+                    </p>
+                </div>
+            </div>
+            <div className="content">
+                <h1> Find a station </h1>
+                <p>
+                    The map shows which stations are open and shows the number of
+                    available bikes in each station in real time.
+                </p>
+                <div className="info-markers">
+                    <div className="station"> <img src="icons/station-orange.png"/> <p>Available bikes</p> </div>
+                    <div className="station"> <img src="icons/station-blue.png"/> <p>Empty Station</p> </div>
+                    <div className="station"> <img src="icons/station-red.png"/> <p>Full Station</p> </div>
+                    <div className="station"> <img src="icons/station-grey.png"/> <p>Out of service</p> </div>
+                </div>
+                <Map/>
+            </div>
         </div>
     );
 }
